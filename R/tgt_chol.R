337d106317e155a34c6ee6549aafae81c7eef919
Julian Collins <julian.collins@kcl.ac.uk>
2019-06-19 10:27:53 +0100

    tgt dependencies

--- R/tgt_chol.R
+++ R/tgt_chol.R
@@ -6,10 +6,10 @@ tgt_chol <- function(x, y) { # x is df to be added to [mutated] # y is numeric c
     stop("Cholesterol input value must be numeric", call. = FALSE)
   }
   else {
-    chol_40 <- as.logical(if_else(x <= 4.0, 1, 0))
-    chol_50 <- as.logical(if_else(x <= 5.0, 1, 0))
-    chol_75 <- as.logical(if_else(x <= 7.5, 1, 0))
-    chol_90 <- as.logical(if_else(x <= 9.0, 1, 0))
+    chol_40 <- as.logical(dplyr::if_else(x <= 4.0, 1, 0))
+    chol_50 <- as.logical(dplyr::if_else(x <= 5.0, 1, 0))
+    chol_75 <- as.logical(dplyr::if_else(x <= 7.5, 1, 0))
+    chol_90 <- as.logical(dplyr::if_else(x <= 9.0, 1, 0))
   }
   return(dplyr::mutate(x, chol_4 = chol_40, chol_5 = chol_50, chol_75 = chol_75, chol_90 = chol_90))
 }
--- R/tgt_hba1c.R
+++ R/tgt_hba1c.R
@@ -6,13 +6,13 @@ tgt_hba1c <- function(x, y) { # x is df to be added to [mutated] # y is numeric 
     stop("HbA1c input value must be numeric", call. = FALSE)
   }
   else {
-    hba1c_48 <- as.logical(if_else(x < 48, 1, 0)) # 6.5%
-    hba1c_53 <- as.logical(if_else(x <= 53, 1, 0)) # 7.0%
-    hba1c_58 <- as.logical(if_else(x <= 58, 1, 0)) # 7.5%
-    hba1c_64 <- as.logical(if_else(x <= 64, 1, 0)) # 8.0%
-    hba1c_69 <- as.logical(if_else(x <= 69, 1, 0)) # 8.5%
-    hba1c_75 <- as.logical(if_else(x <= 75, 1, 0)) # 9.0%
-    hba1c_86 <- as.logical(if_else(x <= 86, 1, 0)) # 10.0%
+    hba1c_48 <- as.logical(dplyr::if_else(x < 48, 1, 0)) # 6.5%
+    hba1c_53 <- as.logical(dplyr::if_else(x <= 53, 1, 0)) # 7.0%
+    hba1c_58 <- as.logical(dplyr::if_else(x <= 58, 1, 0)) # 7.5%
+    hba1c_64 <- as.logical(dplyr::if_else(x <= 64, 1, 0)) # 8.0%
+    hba1c_69 <- as.logical(dplyr::if_else(x <= 69, 1, 0)) # 8.5%
+    hba1c_75 <- as.logical(dplyr::if_else(x <= 75, 1, 0)) # 9.0%
+    hba1c_86 <- as.logical(dplyr::if_else(x <= 86, 1, 0)) # 10.0%
   }
   return(dplyr::mutate(x, hba1c_48 = hba1c_48, hba1c_53 = hba1c_53, hba1c_58 = hba1c_58, hba1c_64 = hba1c_64, hba1c_69 = hba1c_69, hba1c_75 = hba1c_75, hba1c_86 = hba1c_86))
 }
